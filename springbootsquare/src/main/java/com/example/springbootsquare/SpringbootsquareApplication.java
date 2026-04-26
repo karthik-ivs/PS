26730cad18e9eafefc111df43f2f623bfcfce3cf
karthik-ivs <ivskarthik28may2006@gmail.com>
2026-04-26 15:09:44 +0530

    First Commit

---
+++ springbootsquare/src/main/java/com/example/springbootsquare/SpringbootsquareApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsquareApplication.class, args);
+	}
+
+}
---
+++ springbootsquare/src/test/java/com/example/springbootsquare/SpringbootsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
